d140cd2f4b5b52b478166c1e99258110e7e85d93
orlando <orlandomontes97@outlook.com>
2020-09-23 13:46:04 -0500

    Already, was a challenge

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+const viewPlans = document.getElementById("plans");
+const work = document.getElementById("weWork");
+const toggle = document.getElementById("toggle");
+
+viewPlans.addEventListener("click", ()=>alert("Soy un boton para ver planes"));
+work.addEventListener("click", ()=>alert("Seguimos trabajando"));
+toggle.addEventListener("click", ()=>alert("Seguimos trabajando"));
